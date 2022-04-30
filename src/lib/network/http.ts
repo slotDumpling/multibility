87433c3aaf7f138c0ba996420835a8a9ac92ed83
yyb <1033750222@qq.com>
2022-05-01 00:46:34 +0800

    url

--- src/lib/network/http.ts
+++ src/lib/network/http.ts
@@ -4,7 +4,7 @@ import { loadNote, saveTeamNote, updateTeamNote } from "../note/archive";
 import { getUserID } from "../user";
 
 export let BASE_URL = "https://api.slotdumpling.top/paint";
-BASE_URL = "http://100.81.113.84:8090/paint";
+// BASE_URL = "http://100.81.113.84:8090/paint";
 axios.defaults.baseURL = BASE_URL;
 
 axios.interceptors.request.use((config) => {
