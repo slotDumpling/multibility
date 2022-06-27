f9c0ff0b8338e2d8cf51a363a167c4607f46a62a
yyb <1033750222@qq.com>
2022-06-27 15:56:49 +0800

    fix: font color icon dark mode invert

--- src/lib/dark.ts
+++ src/lib/dark.ts
@@ -3,11 +3,16 @@ export const loadDarkMode = async () => {
   setAutoDarkMode(
     { brightness: 100, contrast: 100 },
     {
-      ignoreInlineStyle: [".color-select .circle", ".color-circle"],
+      ignoreInlineStyle: [
+        ".color-select .circle",
+        ".color-circle",
+        ".font-icon",
+      ],
       invert: [
         "[data-force-light=false] .draw-canvas",
         "[data-force-light=false] .color-select .circle",
         "[data-force-light=false] .width-circle.lineWidth",
+        "[data-force-light=false] .font-icon",
         ".note-item .timg",
       ],
       css: "",
