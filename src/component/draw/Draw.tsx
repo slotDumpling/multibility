91fb87423b4e31046291bd224dd1a7749640dd2c
yyb <1033750222@qq.com>
2022-07-03 19:54:37 +0800

    fix: small cursor

--- src/component/draw/Draw.tsx
+++ src/component/draw/Draw.tsx
@@ -614,8 +614,9 @@ const startStroke = (drawCtrl: DrawCtrl) => {
 const cursorStyle = (drawCtrl: DrawCtrl, ratio: number) => {
   const { lineWidth, eraserWidth, mode } = drawCtrl;
   const size = ratio * (mode === "erase" ? eraserWidth : lineWidth);
+  if (size < 5) return "crosshair";
   const half = size / 2;
-  return `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="%23EEE6" width="${size}" height="${size}" viewBox="0 0 10 10"><circle cx="5" cy="5" r="5"/></svg>') ${half} ${half}, auto`;
+  return `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="%23FFF7" width="${size}" height="${size}" viewBox="0 0 10 10"><circle cx="5" cy="5" r="5"/></svg>') ${half} ${half}, auto`;
 };
 
 const moveDash = (item: paper.Item) => {
