fe99c021fbccf226e22baf25f90de9a32ed9fd28
yyb <1033750222@qq.com>
2022-07-05 10:28:13 +0800

    reset test

--- src/component/Test.tsx
+++ src/component/Test.tsx
@@ -1,7 +1,3 @@
 export default function Test() {
-  return (
-    <div style={{ width: 50, backgroundColor: "red" }}>
-      <svg style={{ display: "block" }} viewBox="0 0 100 150" />
-    </div>
-  );
+  return <></>;
 }
