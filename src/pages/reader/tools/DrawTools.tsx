7af2d9440d5a5440a8c1e5d90c813189dafca173
yyb <1033750222@qq.com>
2022-08-20 22:10:13 +0800

    feat: font size

--- src/pages/reader/tools/DrawTools.tsx
+++ src/pages/reader/tools/DrawTools.tsx
@@ -1,10 +1,13 @@
-import { CSSProperties, FC, RefObject, useMemo, useState } from "react";
+import { CSSProperties, FC, RefObject, useState } from "react";
 import {
   CopyOutlined,
+  BoldOutlined,
   DeleteOutlined,
   PictureTwoTone,
   PictureOutlined,
   BgColorsOutlined,
+  ZoomOutOutlined,
+  ZoomInOutlined,
 } from "@ant-design/icons";
 import { Button, ButtonProps, Modal, Popover } from "antd";
 import { DrawCtrl } from "lib/draw/DrawCtrl";
@@ -88,21 +91,18 @@ export const SelectTool: FC<{
 
 export const TextTool: FC<{
   pointText: paper.PointText;
-  drawRef: RefObject<DrawRefType>;
-}> = ({ pointText, drawRef }) => {
+}> = ({ pointText }) => {
   const [text, setText] = useState(pointText.content);
   const [color, setColor] = useState(
     pointText.fillColor?.toCSS(true) ?? allColors[0]!
   );
 
-  const { x, y } = useMemo(() => {
-    const { topLeft } = pointText.bounds;
-    return pointText.view.projectToView(topLeft);
-  }, [pointText]);
+  const { topLeft } = pointText.bounds;
+  const { x, y } = pointText.view.projectToView(topLeft);
 
-  const ratio = pointText.viewMatrix.a;
-  const fontSize = +pointText.fontSize * ratio;
-  const size = pointText.bounds.size.multiply(ratio);
+  const scale = pointText.viewMatrix.a;
+  const fontSize = +pointText.fontSize;
+  const size = pointText.bounds.size.divide(pointText.matrix.a);
   const width = pointText.content ? `calc(${size.width}px + 1em)` : "8em";
   const height = `calc(${size.height}px + 0.2em)`;
 
@@ -126,15 +126,30 @@ export const TextTool: FC<{
     </Popover>
   );
 
+  const [bold, setBold] = useState(pointText.fontWeight === "bold");
+  const fontWeight = bold ? "bold" : "normal";
+  const [, setSize] = useState(0);
+
   return (
     // text-wrapper must be the child of draw-wrapper.
     <>
-      <div className="text-wrapper" style={getPosVars(x, y)}>
+      <div
+        className="text-wrapper"
+        style={
+          {
+            fontSize,
+            "--scale": scale,
+            "--width": width,
+            "--height": height,
+            ...getPosVars(x, y),
+          } as CSSProperties
+        }
+      >
         <textarea
           autoFocus
           placeholder="Insert Text..."
-          style={{ fontSize, color, width, height }}
           value={text}
+          style={{ color, fontWeight }}
           onChange={(e) => {
             const t = e.target.value;
             setText(t);
@@ -144,6 +159,33 @@ export const TextTool: FC<{
       </div>
       <div className="text-options tool-options" style={getPosVars(x, y)}>
         {fontColorBtn}
+        <Button
+          onClick={() => {
+            setBold((prev) => {
+              pointText.fontWeight = prev ? "normal" : "bold";
+              return !prev;
+            });
+          }}
+          {...btnProps}
+          type={bold ? "link" : "text"}
+          icon={<BoldOutlined />}
+        />
+        <Button
+          {...btnProps}
+          icon={<ZoomOutOutlined />}
+          onClick={() => {
+            pointText.scale(0.9, topLeft);
+            setSize((prev) => prev - 1);
+          }}
+        />
+        <Button
+          {...btnProps}
+          icon={<ZoomInOutlined />}
+          onClick={() => {
+            pointText.scale(1.1, topLeft);
+            setSize((prev) => prev + 1);
+          }}
+        />
       </div>
     </>
   );
