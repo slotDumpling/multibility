25ba7f78c0fa2532f955ad5feffd7dc3608508f3
yyb <1033750222@qq.com>
2022-09-12 12:10:09 +0800

    fix: nested

--- src/pages/reader/Info/InfoNav.tsx
+++ src/pages/reader/Info/InfoNav.tsx
@@ -1,6 +1,6 @@
+import { FC, useMemo, useState } from "react";
 import { Input } from "antd";
 import { NoteInfo } from "lib/note/note";
-import { FC, useMemo, useState } from "react";
 import dayjs from "dayjs";
 import calender from "dayjs/plugin/calendar";
 dayjs.extend(calender);
@@ -14,16 +14,18 @@ export const InfoNav: FC<{
   const lastDate = useMemo(() => dayjs(lt).calendar(), [lt]);
 
   return (
-    <nav className="info">
-      <Input
-        className="title"
-        size="large"
-        bordered={false}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        onBlur={() => renameNote(name)}
-      />
-      <p className="time">{lastDate}</p>
+    <nav>
+      <div className="info">
+        <Input
+          className="title"
+          size="large"
+          bordered={false}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onBlur={() => renameNote(name)}
+        />
+        <p className="time">{lastDate}</p>
+      </div>
     </nav>
   );
 };
