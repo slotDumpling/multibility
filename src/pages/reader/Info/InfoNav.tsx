63005b77885e0795caef4ce9f2fa6e62293f9873
yyb <1033750222@qq.com>
2022-09-17 17:27:22 +0800

    feat: input on click, switch create & last time

--- src/pages/reader/Info/InfoNav.tsx
+++ src/pages/reader/Info/InfoNav.tsx
@@ -9,22 +9,41 @@ export const InfoNav: FC<{
   noteInfo: NoteInfo;
   renameNote: (name: string) => void;
 }> = ({ noteInfo, renameNote }) => {
+  const [inputShow, setInputShow] = useState(false);
   const [name, setName] = useState(noteInfo.name);
-  const { lastTime: lt } = noteInfo;
+
+  const [createDateShow, setCreateDateShow] = useState(false);
+  const { lastTime: lt, createTime: ct } = noteInfo;
   const lastDate = useMemo(() => dayjs(lt).calendar(), [lt]);
+  const createDate = useMemo(() => dayjs(ct).calendar(), [ct]);
 
   return (
     <nav>
       <div className="info">
-        <Input
-          className="title"
-          size="large"
-          bordered={false}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          onBlur={() => renameNote(name)}
-        />
-        <p className="time">{lastDate}</p>
+        {inputShow ? (
+          <Input
+            className="title"
+            size="large"
+            bordered={false}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoFocus
+            onBlur={() => {
+              renameNote(name);
+              setInputShow(false);
+            }}
+          />
+        ) : (
+          <p className="title" onClick={() => setInputShow(true)}>
+            {name}
+          </p>
+        )}
+        <p className="time" onClick={() => setCreateDateShow((p) => !p)}>
+          {createDateShow ? createDate : lastDate}
+          <span className="label">
+            {createDateShow ? " Created" : " Last Edited"}
+          </span>
+        </p>
       </div>
     </nav>
   );
