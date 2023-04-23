6946d73fbf0bf94618d68039ff3e9f1c74ef87c5
yyb <1033750222@qq.com>
2023-04-23 15:22:47 +0800

    fix: dark canvas filter => backdrop-filter

--- src/lib/Dark.tsx
+++ src/lib/Dark.tsx
@@ -21,7 +21,6 @@ export const loadDarkMode = async () => {
         ".text-color-icon",
       ],
       invert: [
-        ".draw-canvas",
         "label .circle",
         ".width-circle.lineWidth",
         ".pen-icon",
