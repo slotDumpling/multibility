914ef9840a30d1b0a7f9b5acb4fcb11757267ec0
yyb <1033750222@qq.com>
2023-07-02 16:32:22 +0800

    fix: intro localstorage key

--- src/lib/note/intro.ts
+++ src/lib/note/intro.ts
@@ -4,7 +4,6 @@ import { createEmptyNote } from "./note";
 export async function createIntroNote() {
   const key = "INTRO_CREATED";
   if (localStorage.getItem(key)) return;
-  localStorage.setItem(key, "CREATED");
 
   const note = createEmptyNote();
   note.name = "Welcome to Multibility!";
@@ -13,5 +12,6 @@ export async function createIntroNote() {
   if (!firstPageRec) return;
   const { default: state } = await import("./introState.json");
   firstPageRec.state = JSON.parse(state);
+  localStorage.setItem(key, "CREATED");
   return await createNewNote(note);
 }
